7fe01ef9c541ef643d886e2fd28588d14f5817b5
Cristo <cristo.rabani@gmail.com>
2023-03-03 06:21:50 +0100

    ignore

--- lib/typescript/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type RunningMode = 'LOCAL' | 'REMOTE';
-declare type BundleManagerType = {
-    setPackagerHost(host: string): Promise<void>;
-    load(url: string): Promise<void>;
-    runningMode(): Promise<RunningMode>;
-};
-declare const _default: BundleManagerType;
-export default _default;
